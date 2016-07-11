03a1edd093969a7ce711eb4b3405a068f095756c
samirwdhw <samir.wdhw@gmail.com>
2016-07-11 17:02:58 +0530

    Ball detected

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ def nothing(x):
 cap = cv2.VideoCapture(0)
 
 cv2.namedWindow('video')
-
+cv2.namedWindow('detail')
 
 #Creating the trackbar
-cv2.createTrackbar('H', 'video', 0, 179, nothing)
+cv2.createTrackbar('H', 'video', 10, 179, nothing)
+
+cv2.createTrackbar('param2', 'detail', 0, 200, nothing)
 
 #Some default values (For yellow smiley ball)
-FINAL_MIN = np.array([30-10, 50, 50])
+FINAL_MIN = np.array([30-10, 100, 100])
 FINAL_MAX = np.array([30+10, 255, 255])
 
 
 #To accept the value for H that is suitable for the ball
+
 
 while(1):
 
@@ -52,6 +55,52 @@ while(1):
 		FINAL_MAX = COLOR_MAX	
 		break
 
+#To detect the ball
+while(1):
+	
+	ret, frame = cap.read()
+
+	#Change to HSV
+	hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+	#Threshold according to values 
+	frame_threshold = cv2.inRange(hsv_frame, FINAL_MIN, FINAL_MAX)
+
+	frame_threshold = cv2.medianBlur(frame_threshold, 5)
+
+	frame_threshold = cv2.erode(frame_threshold, None, iterations = 2)
+	frame_threshold = cv2.dilate(frame_threshold, None, iterations = 2)
+
+	#Hough circle transform
+	
+	cnts = cv2.findContours(frame_threshold.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	
+	center = None
+
+	if len(cnts) > 0:
+		
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		
+		M = cv2.moments(c)
+		
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		
+		cv2.circle(frame, (int(x), int(y)), int(radius),
+				(0, 255, 255), 2)
+		
+		cv2.circle(frame, center, 5, (0, 0, 255), -1)
+	
+
+	cv2.imshow('video', frame)
+	cv2.imshow('detail',frame_threshold)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
 
 
